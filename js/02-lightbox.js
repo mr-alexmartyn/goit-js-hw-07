131f63eaacfdb8c02821a6273ab91a1eca397eda
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-01-21 21:08:31 +0200

    galleryLightbox create

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const imageMarkup = createMarkupPhotos(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createMarkupPhotos(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionPosition: `bottom`,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
